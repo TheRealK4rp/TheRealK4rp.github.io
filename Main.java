839a9c671fe35f0d08044084a799e70a05a8a7d3
Joel Tsang <joeltsang@Joels-MacBook-Pro.local>
2025-04-01 21:29:44 -0700

    I THINK IT WORKS

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
+import java.io.*;
+import java.util.Scanner;
 public class Main{
     public static void main(String[] args) {
+        printAsa();
+    }
+
+    public static void printHTML(){
         int day = 1;
         for(int i = 0; i<140;i++){
             if(i%4==0){
@@ -10,6 +16,29 @@ public class Main{
                 }
             }
             System.out.println("\t<img src=\"\" id = \"temp" +i+"\" width=\"85\" height =\"85\">");
+        }
+    }
+
+    public static void printAsa(){
+        try{
+            File file = new File("asa.txt");
+            FileInputStream in = new FileInputStream(file);
+            Scanner s = new Scanner(in);
+            for(int i = 0;i<30;i++){
+                String line = s.nextLine().toLowerCase();
+                if(line.equals("")) line = ",,";
+                else{
+                    int a = line.indexOf(" ");
+                    if(a<0) line = "\""+line + ".jpg\",,";
+                    else line = "\"" + line.substring(0,a) + ".jpg\",\"" + line.substring(a+1)+".jpg\"";
+                }
+                System.out.print(","+line+",");
+            }
+            s.close();
+
+        }
+        catch(Exception e){
+            e.printStackTrace();
         }
     }
 }
