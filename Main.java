c9905db0e43c5a1e383e1d0e669a42d76a517d82
TheRealK4rp <joeljobtsang@gmail.com>
2025-04-02 00:11:04 -0700

    yoru done i think

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.Scanner;
 public class Main{
     public static void main(String[] args) {
-        printAsa();
+        printYoru();
     }
 
     public static void printHTML(){
@@ -30,7 +30,7 @@ public class Main{
                 else{
                     int a = line.indexOf(" ");
                     if(a<0) line = "\""+line + ".jpg\",,";
-                    else line = "\"" + line.substring(0,a) + ".jpg\",\"" + line.substring(a+1)+".jpg\"";
+                    else line = "\"" + line.substring(0,a) + ".jpg\",\"" + line.substring(a+1)+".jpg\",";
                 }
                 System.out.print(","+line+",");
             }
@@ -41,4 +41,37 @@ public class Main{
             e.printStackTrace();
         }
     }
+
+    public static void printYoru(){
+        try{
+            File file = new File("yoru.txt");
+            FileInputStream in = new FileInputStream(file);
+            Scanner s = new Scanner(in);
+            for(int i = 0;i<30;i++){
+                String line = s.nextLine().toLowerCase();
+                if(line.equals("")) line = ",,,,";
+                else{
+                    line = "\"" + line;
+                    for(int b = 0; b<4;b++){
+                        int a = line.indexOf(" ");
+                        if(a < 0){
+                            line+=".jpg\"";
+                            for(int c = 0; c < 4-b; c++){
+                                line+=",";
+                            }
+                            break;
+                        }
+                        line = line.substring(0,a)+".jpg\",\""+line.substring(a+1);
+                    }
+                }
+                System.out.print(line);
+            }
+            s.close();
+
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
 }
