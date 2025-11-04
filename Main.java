8694e914f93882db831c407dea2e0a89e4f42b4b
Joel Tsang <joeltsang@joels-mbp.lan>
2025-11-04 14:00:59 -0800

    fix

--- Main.java
+++ Main.java
@@ -245,7 +245,7 @@ public class Main{
                             int d = 4-b-1;
                             boolean f = false;
                             //day - 1 to add one
-                            if(!(((i+dayOne)%7 == 0 || (i+dayTwo)%7 == 0 || (i+dayThree) % 7 == 0) ) ){ 
+                            if(!(((i+dayOne)%7 == 0 || (i+dayTwo)%7 == 0 || (i+dayThree) % 7 == 0) ) || i ==4  ){ 
                                 d--;
                                 f=true;
                             }
