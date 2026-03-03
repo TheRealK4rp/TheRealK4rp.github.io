4741789f6606575512b4e08a48555f98d11e8ad0
TheRealK4rp <joeljobtsang@gmail.com>
2026-03-03 04:09:41 -0800

    a

--- Main.java
+++ Main.java
@@ -250,7 +250,7 @@ public class Main{
                             int d = 4-b-1;
                             boolean f = false;
                             //day - 1 to add one
-                            if(!(((i+dayOne)%7 == 0 || (i+dayTwo)%7 == 0 || (i+dayThree) % 7 == 0) ) && i != 1 ){ 
+                            if(!(((i+dayOne)%7 == 0 || (i+dayTwo)%7 == 0 || (i+dayThree) % 7 == 0) ) && i != 1 && i != 4 ){ 
                                 d--;
                                 f=true;
                             }
